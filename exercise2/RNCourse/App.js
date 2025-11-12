7072f33166378787422aad3639d5ecdec2563c9d
HoTienQuoc <hotienquoc0429@gmail.com>
2025-11-12 21:50:58 +0700

    Refactor App.js to improve layout and functionality. Replaced dummy text with a TextInput for user input and an 'Add Goal' button. Updated styles for better presentation.

--- exercise2/RNCourse/App.js
+++ exercise2/RNCourse/App.js
@@ -1,30 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={styles.appContainer}>
       <View>
-        <Text style={styles.dummpyText}>Open up App.js to start working on your app!</Text>
+        <TextInput placeholder='Your course goal!' />
+        <Button title='Add Goal' />
       </View>
-      <StatusBar style="auto" />
-      <Text style={styles.dummpyText}>Hello World</Text>
-      <Button title='Tap me!'/>
+      <View>
+        <Text></Text>
+      </View>
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  dummpyText: {
-    margin:16,
-    padding:16,
-    borderWidth:2,
-    borderColor:'red'
+  appContainer: {
+    padding: 16
   }
 });
