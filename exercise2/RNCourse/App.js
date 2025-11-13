aa28b2d451a00dd3f6c92e466578d39dee2b8559
HoTienQuoc <hotienquoc0429@gmail.com>
2025-11-13 11:35:52 +0700

    Revamp App.js layout by replacing input field and button with a new grid of colored boxes. Each box displays a number, enhancing visual structure and interactivity.

--- exercise2/RNCourse/App.js
+++ exercise2/RNCourse/App.js
@@ -3,13 +3,39 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder='Your course goal!' />
-        <Button title='Add Goal' />
+    <View style={{padding:50, flexDirection:'row-reverse', width:'80%', height:'300'}}>
+      <View
+        style={{
+          backgroundColor: 'red',
+          width: 100,
+          height: 100,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>1</Text>
       </View>
-      <View>
-        <Text>List of goals...</Text>
+      <View
+        style={{
+          backgroundColor: 'blue',
+          width: 100,
+          height: 100,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>2</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: 'green',
+          width: 100,
+          height: 100,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>3</Text>
       </View>
     </View>
 
